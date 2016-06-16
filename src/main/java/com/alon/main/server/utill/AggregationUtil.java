b559f9b0c11aaaabbc3453bafe4656c449c19b23
alon_ss <alon.sarshalom@gmail.com>
2016-06-16 14:58:55 +0300

    add second aggregation strategy

--- src/main/java/com/alon/main/server/utill/AggregationUtil.java
+++ src/main/java/com/alon/main/server/utill/AggregationUtil.java
@@ -25,12 +25,7 @@ public class AggregationUtil {
 
     private static String combineLogic(Set<JSONObject> jsonObjectSet) {
         JSONObject jsonObjectResponse = new JSONObject();
-
-        for (JSONObject json: jsonObjectSet){
-            for (String key: json.keySet()){
-                jsonObjectResponse.put(key, json.get(key));
-            }
-        }
+        jsonObjectSet.forEach(x -> addToJson(jsonObjectResponse, x));
 
         return jsonObjectResponse.toString();
     }
@@ -42,4 +37,39 @@ public class AggregationUtil {
         }
         return json.toString();
     }
+
+    /*
+     Implementation that add a json keys and values to the "master" Json
+     */
+    private static void addToJson(JSONObject masterJson, JSONObject jsonToAdd) {
+        jsonToAdd.keySet().forEach(key -> masterJson.put(key, jsonToAdd.get(key)));
+    }
+
+//    /*
+//     Implementation that take a json and strip it to its smallest values
+//      and then add it keys and values to the "master" Json
+//    */
+//    private static void addToJson(JSONObject masterJson, JSONObject jsonToAdd) {
+//        jsonToAdd.keySet().forEach(key -> flattObject(masterJson, key, jsonToAdd.get(key)));
+//    }
+//
+//    private static void flatten(JSONObject masterJson, JSONObject jsonToFlatt) {
+//        for (String key: jsonToFlatt.keySet()){
+//            Object value = jsonToFlatt.get(key);
+//            flattObject(masterJson, key, value);
+//        }
+//    }
+//
+//    private static void flattObject(JSONObject masterJson, String key, Object value) {
+//        if (value instanceof JSONObject){
+//            flatten(masterJson, (JSONObject)value);
+//        }else if (value instanceof JSONArray){
+//            JSONArray jsonArray = ((JSONArray) value);
+//            for (Object obj :jsonArray){
+//                flattObject(masterJson, key, obj);
+//            }
+//        }else{
+//            masterJson.put(key, value);
+//        }
+//    }
 }
